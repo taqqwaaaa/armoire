19db2b360ebbcba0dd672653456194122ff19094
ameya-g-git <121361927+ameya-g-git@users.noreply.github.com>
2025-03-26 13:27:56 -0400

    initialized fonts

--- login/index.php
+++ login/index.php
@@ -12,7 +12,24 @@
 </head>
 
 <body>
-    <form action="login.php"></form>
+    <div>
+
+        <h1 id="heading">Log-in</h1>
+        <div>
+            <button>Log-in</button>
+            <button>Create Account</button>
+        </div>
+        <form action="login.php" method="post">
+            <fieldset>
+                <legend>Username</legend>
+                <input type="text" name="username" id="username">
+            </fieldset>
+            <fieldset>
+                <legend>Password</legend>
+                <input type="password" name="password" id="password">
+            </fieldset>
+        </form>
+    </div>
 </body>
 
 </html>
